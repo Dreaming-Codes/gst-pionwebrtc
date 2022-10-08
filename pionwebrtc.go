ca36aa20cc3a21fcca0a8a70939e166419356de7
DreamingCodes <dev@dreaming.codes>
2022-10-08 20:51:30 +0200

    Added gst properties

--- pionwebrtc.go
+++ pionwebrtc.go
@@ -16,10 +16,33 @@ var CAT = gst.NewDebugCategory(
 	"PionWebRTC Element",
 )
 
+var properties = []*glib.ParamSpec{}
+
 func main() {}
 
 type gobin struct{}
 
 func (g *gobin) New() glib.GoObjectSubclass { return &gobin{} }
 
-func (g *gobin) ClassInit(klass *glib.ObjectClass) {}
+func (g *gobin) ClassInit(klass *glib.ObjectClass) {
+	class := gst.ToElementClass(klass)
+	class.SetMetadata(
+		"PionWebRTC Element",
+		"Bin/WebRTC",
+		"WebRTC Video and Audio Transmitter",
+		"Lorenzo Rizzotti <dev@reaming.codes>",
+	)
+	class.AddPadTemplate(gst.NewPadTemplate(
+		"src",
+		gst.PadDirectionSource,
+		gst.PadPresenceSometimes,
+		gst.NewAnyCaps(),
+	))
+	class.AddPadTemplate(gst.NewPadTemplate(
+		"sink",
+		gst.PadDirectionSink,
+		gst.PadPresenceRequest,
+		gst.NewAnyCaps(),
+	))
+	class.InstallProperties(properties)
+}
